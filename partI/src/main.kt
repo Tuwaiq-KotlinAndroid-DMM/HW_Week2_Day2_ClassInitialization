0ee9233eebf486cbed309be9581b9ef3be064409
Mustafa <Mustafa.Haassen.dmm.k1@tuwaiq.edu.sa>
2021-10-04 21:52:45 +0300

    added solution for partI

---
+++ partI/src/main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("Enter the number of students:")
+    var x = Student("","",0)
+    println(x.displayTest())
+
+    println(x.search())
+
+    println(x.getAvr())
+
+    println(x.honList())
+}
