1a5e907c276399b821b1a284f0a6138fa3787b23
ibtihala817 <Ibtihal.ibrahim.dmm.k1@tuwaiq.edu.sa>
2021-10-05 06:21:32 +0300

    hw6

---
+++ HW6/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(){
+        println("Enter the number of students:")
+        var x = Student("","",0)
+        println(x.displayTest())
+
+        println(x.search())
+
+        println(x.getAvr())
+
+        println(x.honList())
+}
+
+
+
+
+
+
---
+++ HW6/src/main/kotlin/class.kt
@@ -0,0 +1,65 @@
+class Student(var fName:String,
+              var lName:String,
+              var testScore:Int
+) {
+    var input = readLine()!!.toInt()
+    var studentMap = mutableMapOf<String,Int>()
+    lateinit var studentName:String
+
+
+
+    init{
+        fName = ""
+        lName = ""
+        testScore = 0
+    }
+    fun addTests(){
+
+        var i = 1
+        while (i<input+1){
+            println("Enter first Name:")
+            fName = readLine()!!
+            println("Enter last Name:")
+            lName = readLine()!!
+            studentName = ("${fName.capitalize()} ${lName.capitalize()}")
+            println("Enter test score:")
+            testScore = readLine()!!.toInt()
+            while (testScore < 0 || testScore > 100){
+                println("test score must be between 0 - 100")
+                testScore = readLine()!!.toInt()
+            }
+            studentMap[studentName] = testScore
+            i++
+            println("the map is: $studentMap")
+
+        }
+    }
+
+    fun displayTest(){
+        println(addTests())
+        println("the list is: ${studentMap.toList()}")
+    }
+
+    fun search(){
+        println("write student name to search about his result:")
+        println("Warning!! -- First letter of first & last name must be capital letter!!")
+        println(studentMap[readLine()!!])
+    }
+
+
+    fun getAvr(){
+        var avr = studentMap.values
+        println("the class average is: ${avr.sum()/input}")
+
+    }
+
+    fun honList() {
+        var honorList = mutableListOf<String>()
+        studentMap.forEach { t, u ->
+            if (u > 90){
+                honorList.add(t)
+
+            }
+        }
+    }
+}
